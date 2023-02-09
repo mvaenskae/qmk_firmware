b4bc4e90406300929c1a8cbc11c8d826023c3786
Mickey Vänskä <mvaenskae@gmail.com>
2023-02-09 22:57:01 +0100

    Refactor RGB Matrix config.h section
    Signed-off-by: Mickey Vänskä <mvaenskae@gmail.com>

--- keyboards/sofle/keymaps/mvaenskae/config.h
+++ keyboards/sofle/keymaps/mvaenskae/config.h
@@ -88,49 +88,68 @@
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
-#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+	#define RGB_MATRIX_LED_COUNT 35
+	#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+	#define RGB_MATRIX_FRAMEBUFFER_EFFECTS // enable framebuffer effects
+	#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+	#define RGB_MATRIX_TIMEOUT 0 // number of milliseconds to wait until rgb automatically turns off
+	#define RGB_MATRIX_SPLIT
+	#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
+	#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+	#define RGB_MATRIX_DEFAULT_HUE 0
+	#define RGB_MATRIX_DEFAULT_SAT 0
+	#define RGB_MATRIX_DEFAULT_VAL 0
+	#define RGB_MATRIX_DEFAULT_SPD 0
 
-#    define RGB_MATRIX_HUE_STEP 8
-#    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
-#    define RGB_MATRIX_SPD_STEP 10
+	#define RGB_MATRIX_HUE_STEP 8
+	#define RGB_MATRIX_SAT_STEP 8
+	#define RGB_MATRIX_VAL_STEP 8
+	#define RGB_MATRIX_SPD_STEP 10
 
-/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
- * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
- // #   undef ENABLE_RGB_MATRIX_ALPHAS_MODS
- // #   undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
- // #   undef ENABLE_RGB_MATRIX_BREATHING
- // #   undef ENABLE_RGB_MATRIX_CYCLE_ALL
- // #   undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
- // #   undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
- // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
- // #   undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
- // #   undef ENABLE_RGB_MATRIX_DUAL_BEACON
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
- // #   undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
- // #   undef ENABLE_RGB_MATRIX_RAINDROPS
- // #   undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
- // #   undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
- // #   undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
- // #   undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
- // #   undef ENABLE_RGB_MATRIX_SPLASH
- // #   undef ENABLE_RGB_MATRIX_MULTISPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_SPLASH
- // #   undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+	#undef ENABLE_RGB_MATRIX_ALPHAS_MODS            // Static dual hue, speed is hue for secondary hue
+	#undef ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN       // Static gradient top to bottom, speed controls how much gradient changes
+	#undef ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT    // Static gradient left to right, speed controls how much gradient changes
+	#undef ENABLE_RGB_MATRIX_BREATHING              // Single hue brightness cycling animation
+	#undef ENABLE_RGB_MATRIX_BAND_SAT               // Single hue band fading saturation scrolling left to right
+	#undef ENABLE_RGB_MATRIX_BAND_VAL               // Single hue band fading brightness scrolling left to right
+	#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT      // Single hue 3 blade spinning pinwheel fades saturation
+	#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL      // Single hue 3 blade spinning pinwheel fades brightness
+	#undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT        // Single hue spinning spiral fades saturation
+	#undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL        // Single hue spinning spiral fades brightness
+	#undef ENABLE_RGB_MATRIX_CYCLE_ALL              // Full keyboard solid hue cycling through full gradient
+	#undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT       // Full gradient scrolling left to right
+	#undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN          // Full gradient scrolling top to bottom
+	#undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN           // Full gradient scrolling out to in
+	#undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL      // Full dual gradients scrolling out to in
+	#undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON // Full gradient Chevron shapped scrolling left to right
+	#undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL         // Full gradient spinning pinwheel around center of keyboard
+	#undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL           // Full gradient spinning spiral around center of keyboard
+	#undef ENABLE_RGB_MATRIX_DUAL_BEACON            // Full gradient spinning around center of keyboard
+	#undef ENABLE_RGB_MATRIX_RAINBOW_BEACON         // Full tighter gradient spinning around center of keyboard
+	#undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS      // Full dual gradients spinning two halfs of keyboard
+	#undef ENABLE_RGB_MATRIX_RAINDROPS              // Randomly changes a single key's hue
+	#undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS    // Randomly changes a single key's hue and saturation
+	#undef ENABLE_RGB_MATRIX_HUE_BREATHING          // Hue shifts up a slight ammount at the same time, then shifts back
+	#undef ENABLE_RGB_MATRIX_HUE_PENDULUM           // Hue shifts up a slight ammount in a wave to the right, then back to the left
+	#undef ENABLE_RGB_MATRIX_HUE_WAVE               // Hue shifts up a slight ammount and then back down in a wave to the right
+	#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL          // Single hue fractal filled keys pulsing horizontally out to edges
+	#undef ENABLE_RGB_MATRIX_PIXEL_FLOW             // Pulsing RGB flow along LED wiring with random hues
+	#undef ENABLE_RGB_MATRIX_PIXEL_RAIN             // Randomly light keys with random hues
+
+	#undef ENABLE_RGB_MATRIX_TYPING_HEATMAP // How hot is your WPM!
+	#undef ENABLE_RGB_MATRIX_DIGITAL_RAIN   // That famous computer simulation
+
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE     // Pulses keys hit to hue & value then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE            // Static single hue, pulses keys hit to shifted hue then fades to current hue
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE       // Hue & value pulse near a single key hit then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE  // Hue & value pulse near multiple key hits then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS      // Hue & value pulse the same column and row of a single key hit then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS // Hue & value pulse the same column and row of multiple key hits then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS      // Hue & value pulse away on the same column and row of a single key hit then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS // Hue & value pulse away on the same column and row of multiple key hits then fades value out
+	#undef ENABLE_RGB_MATRIX_SPLASH                    // Full gradient & value pulse away from a single key hit then fades value out
+	#undef ENABLE_RGB_MATRIX_MULTISPLASH               // Full gradient & value pulse away from multiple key hits then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_SPLASH              // Hue & value pulse away from a single key hit then fades value out
+	#undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH         // Hue & value pulse away from multiple key hits then fades value out
 #endif
